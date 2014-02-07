aaf95e0aea6e5231663ee3a00b32dd61b76d686e
Tyson Pitt <tysonpitt@gmail.com>
2014-02-07 12:46:56 -0500

    Add user microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,51 +1,53 @@
 require 'spec_helper'
 
-	describe User do
+describe User do
 
-	  before do
-	  	@user = User.new(name: "Example User", email: "user@example.com",
-	  									 password: "foobar", password_confirmation: "foobar")
-	  end
+  before do
+  	@user = User.new(name: "Example User", email: "user@example.com",
+  									 password: "foobar", password_confirmation: "foobar")
+  end
 
-	  subject { @user }
+  subject { @user }
 
-	  it { should respond_to(:name) }
-	  it { should respond_to(:email) }
-	  it { should respond_to(:password_digest) }
-	  it { should respond_to(:password) }
-	  it { should respond_to(:password_confirmation) }
-    it { should respond_to(:remember_token) }
-	  it { should respond_to(:authenticate) }
-    it { should respond_to(:admin) }
-	
-		it { should be_valid }
-    it { should_not be_admin }
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+  it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
 
-    describe "with admin attribute set to 'true'" do
-      before do
-        @user.save!
-        @user.toggle!(:admin)
-      end
+	it { should be_valid }
+  it { should_not be_admin }
 
-      it { should be_admin }
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
     end
 
-		describe "when name is not present" do
-			before { @user.name = " " }
-			it { should_not be_valid }
-		end
+    it { should be_admin }
+  end
 
-		describe "when email is not present" do
-			before { @user.email = " " }
-			it { should_not be_valid }
-		end
+	describe "when name is not present" do
+		before { @user.name = " " }
+		it { should_not be_valid }
+	end
 
-		describe "when name is too long" do
-			before { @user.name = "a" * 51 }
-			it { should_not be_valid }
-		end
+	describe "when email is not present" do
+		before { @user.email = " " }
+		it { should_not be_valid }
+	end
 
-		describe "when email format is invalid" do
+	describe "when name is too long" do
+		before { @user.name = "a" * 51 }
+		it { should_not be_valid }
+	end
+
+	describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com foo@bar..com]
@@ -123,5 +125,40 @@ require 'spec_helper'
   describe "remember token" do
     before { @user.save }
     its(:remember_token)  { should_not be_blank }
+  end
+
+  describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.to_a
+      @user.destroy
+      expect(microposts).not_to be_empty
+      microposts.each do |micropost|
+        expect(Micropost.where(id: micropost.id)).to be_empty
+      end
+    end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+    end
+  
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,6 +9,12 @@ describe "Authentication" do
 
   	it { should have_content('Sign in') }
   	it { should have_title('Sign in') }
+
+    describe "for non-signed-in users" do
+      it { should_not have_link('Users') }
+      it { should_not have_link('Profile') }
+      it { should_not have_link('Settings') }
+    end
   end
 
   describe "signin" do
@@ -28,11 +34,7 @@ describe "Authentication" do
 
   	describe "with valid information" do
   		let(:user)	{ FactoryGirl.create(:user) }
-  		before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+  		before { sign_in user }
 
   		it { should have_title(user.name) }
       it { should have_link('Users',        href: users_path)   }
@@ -56,15 +58,25 @@ describe "Authentication" do
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
+          sign_in user
         end
 
         describe "after signing in" do
 
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
+          end
+
+          describe "when signing in again" do
+            before do
+              click_link "Sign out"
+              visit signin_path
+              sign_in user
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
           end
         end
       end
@@ -84,6 +96,19 @@ describe "Authentication" do
         describe "visiting the user index" do
           before { visit users_path }
           it { should have_title('Sign in') }
+        end
+      end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
         end
       end
     end
